701f42e7dea129ffbd4eebcd8cb96aad81538b25
Izaskun Monfort <imonfort001@gmail.com>
2025-02-06 13:42:19 +0000

    create MovieSceneList fucntion

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,20 +2,21 @@ import "../styles/App.scss";
 import "../components/Layout/Header";
 import Header from "../components/Layout/Header";
 import Footer from "../components/Layout/Footer";
+import { useState } from "react";
+import MovieSceneList from "../components/MovieSceneList";
 
 function App() {
+
+  // VARIABLES DE ESTADO
+
+  const [movie, setMovie] = useState()
+
   return (
     <>
       <Header></Header>
       <main>
-                <section className="list">
-          <ul className="list__ul">
-            <li className="list__container">
-              <img src="" alt="" className="list__container-movieImg" />
-              <p className="list__container-movieData"></p>
-              <h3 className="list__container-wowLine"></h3>
-            </li>
-          </ul>
+        <section className="list">
+        <MovieSceneList movie={movie}></MovieSceneList>  
         </section>
       </main>
       <Footer></Footer>
